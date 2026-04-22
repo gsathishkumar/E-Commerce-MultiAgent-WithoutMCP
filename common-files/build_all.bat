14b4e0e6861738e9d904b5342dbba752404513ef
Sathishkumar <be.sathish@gmail.com>
2026-04-22 21:17:17 +0530

    Changed the tag for each docker image

--- common-files/build_all.bat
+++ common-files/build_all.bat
@@ -1,12 +1,12 @@
 cd ./../product-rag-api
-docker build -t ecommerce/product_rag_api:latest .
+docker build -t ecommerce/product_rag_api:without-mcp .
 cd ./../refund-rag-api
-docker build -t ecommerce/refund_rag_api:latest .
+docker build -t ecommerce/refund_rag_api:without-mcp .
 cd ./../order-api
-docker build -t ecommerce/order_mongo_app:latest .
+docker build -t ecommerce/order_mongo_app:without-mcp .
 cd ./../refund-api
-docker build -t ecommerce/refund_mongo_app:latest .
+docker build -t ecommerce/refund_mongo_app:without-mcp .
 cd ./../ecommerce-support
-docker build -t ecommerce/ecommerce_support_app:latest .
+docker build -t ecommerce/ecommerce_support_app:without-mcp .
 cd ./../common-files
 
